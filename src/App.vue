83d52526db6feaf737051d4ec1d2c33d6cc1b1e5
海洋 丁 <3023265978@qq.com>
2024-10-10 21:20:21 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,18 @@
 <template>
   <!-- 一级路由出口组件 -->
    <router-view/>
+   <div class="test">
+    test scss
+   </div>
 </template> 
 
-<style scoped>
+<style scoped lang="scss">
+// @import '@/styles/var.scss';
+
+.test{
+color: $priceColor;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
