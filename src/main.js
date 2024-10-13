4832b7277a88e656d814548979016e426af1e737
海洋 丁 <3023265978@qq.com>
2024-10-13 14:50:47 +0800

    Layout吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ import App from './App.vue'
 import router from './router'
 
 
-//测试接口函数
-import {getCategory} from '@/apis/testAPI'
-import { colProps } from 'element-plus'
-getCategory().then(res=>{
-    console.log('res', res)
-})
+// //测试接口函数
+// import {getCategory} from '@/apis/testAPI'
+// import { colProps } from 'element-plus'
+// getCategory().then(res=>{
+//     console.log('res', res)
+// })
     
 const app = createApp(App)
 
@@ -22,3 +22,4 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+    
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,10 +2,12 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFived from './components/LayoutFived.vue';
 </script>
 
 <template>
     <div>
+        <LayoutFived/>
         <LayoutNav />
         <LayoutHeader />
         <!-- 二级路由出口 -->
