979bf3320716a5729d0ba9241cd403276f1d9384
海洋 丁 <3023265978@qq.com>
2024-10-18 14:50:08 +0800

    解决useIntersectionObserver重复监听浪费内存的问题

--- src/direcyives/index.js
+++ src/direcyives/index.js
@@ -10,7 +10,7 @@ export const lazyPlugin={
                 //el：指令绑定的那个元素 img
                 //binding：binding.value  指令等于号后面绑定的表达式的值 图片url
                 // console.log(el,binding.value)
-                useIntersectionObserver(
+                const {stop}=useIntersectionObserver(
                     el,
                     ([{ isIntersecting }]) => {
                     //   console.log(isIntersecting)
