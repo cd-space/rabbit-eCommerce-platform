b2be19e5b7d31e38456508bf480a48dc8746e6aa
海洋 丁 <3023265978@qq.com>
2024-10-19 18:33:02 +0800

    home- 轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import {getTopCategoryAPI} from'@/apis/catagory'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import {getBannerAPI} from'@/apis/home'
 
 //获取数据
 const catagoryData = ref({})
@@ -16,20 +17,42 @@ onMounted(() => {
 })
 
 
+//获取banner
+const bannerList =ref([])
+
+const getBanner=async()=>{
+    const res =await getBannerAPI({
+        distributionSite:'2'
+    })
+    // console.log(res)
+    bannerList.value=res.result
+}
+
+onMounted(()=>getBanner())
+
+
 </script>
 
 <template>
-  <div class="top-category">
-    <div class="container m-top-20">
-      <!-- 面包屑 -->
-      <div class="bread-container">
-        <el-breadcrumb separator=">">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{ catagoryData.name }}</el-breadcrumb-item>
-        </el-breadcrumb>
-      </div>
+    <div class="top-category">
+        <div class="container m-top-20">
+            <!-- 面包屑 -->
+            <div class="bread-container">
+                <el-breadcrumb separator=">">
+                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                    <el-breadcrumb-item>{{ catagoryData.name }}</el-breadcrumb-item>
+                </el-breadcrumb>
+            </div>
+            <!-- 轮播图 -->
+            <div class="home-banner">
+                <el-carousel height="500px">
+                    <el-carousel-item v-for="item in bannerList" :key="item.id">
+                        <img :src="item.imgUrl" alt="">
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 
@@ -109,6 +132,15 @@ onMounted(() => {
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
